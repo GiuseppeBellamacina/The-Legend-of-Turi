2020c8e51c615736e4f378745e7ed2bf484dc356
Giuseppe Bellamacina <giuseppe.bellamacina@studium.unict.it>
2024-05-22 20:15:01 +0200

    Aggiornamento a Turi9

--- Scripts/Characters/Enemies/Smile.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Slime : Enemy
-{
-    [Header("Slime Settings")]
-    public GameObject projectile;
-}
--- Scripts/ScriptableObjects/Manageable.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class Manageable : ScriptableObject
-{
-    public virtual void Reset() {}
-}
